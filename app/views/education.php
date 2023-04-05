c99157b399d1487c30539bd33d08eba2282960f6
Eduard  Rino Carton <56856915+awtsmahalko@users.noreply.github.com>
2023-04-05 15:49:53 +0800

    Update education.php

--- app/views/education.php
+++ app/views/education.php
@@ -15,8 +15,8 @@ ENGINE=InnoDB
 AUTO_INCREMENT=3
 ;
  -->
- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7/dist/sweetalert2.all.min.js"></script>
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7/dist/sweetalert2.min.css">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7/dist/sweetalert2.all.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7/dist/sweetalert2.min.css">
 <!-- ============================ Page Title Start================================== -->
 <div class="page-title bg-cover" style="background:url(../assets/img/front_bg.webp)no-repeat;" data-overlay="5">
 	<div class="container">
@@ -63,9 +63,8 @@ AUTO_INCREMENT=3
 								</div>
 								<div class="col-md-6">
 									<button type="button" class="btn btn-sm btn-primary" id="btn_update_profile"
-										style="float: right !important;" data-toggle="modal"
-										data-target="#modalAddEduc"><span class="fa fa-plus"></span> Add
-										Education</button>
+										style="float: right !important;" data-toggle="modal" data-target="#modalAddEduc"
+										onclick="resetFields()"><span class="fa fa-plus"></span> Add Education</button>
 
 								</div>
 							</div>
@@ -101,6 +100,7 @@ AUTO_INCREMENT=3
 </section>
 
 <form id="frmEducation">
+	<input type="hidde" name="educ_id" data-column="educ_id">
 	<!-- Log In Modal -->
 	<div class="modal fade" id="modalAddEduc" tabindex="-1" role="dialog" aria-labelledby="modaladdeduc"
 		aria-hidden="true">
@@ -116,32 +116,36 @@ AUTO_INCREMENT=3
 						<div class="col-xl-12 col-lg-12">
 							<div class="form-group">
 								<label>Qualifications</label>
-								<input type="text" class="form-control" name='educ_degree'
-									placeholder="Bachelor of Science in " required>
+								<input type="text" class="form-control educ-value" data-column="educ_degree"
+									name='educ_degree' placeholder="Bachelor of Science in " required>
 							</div>
 						</div>
 						<div class="col-xl-12 col-lg-12">
 							<div class="form-group">
 								<label>Institution</label>
-								<input type="text" class="form-control" name='educ_school' placeholder="NONESCOST" required>
+								<input type="text" class="form-control educ-value" data-column="educ_school"
+									name='educ_school' placeholder="NONESCOST" required>
 							</div>
 						</div>
 						<div class="col-xl-12 col-lg-12">
 							<div class="form-group">
 								<label>Honors Received</label>
-								<input type="text" class="form-control" name='honor_received' placeholder="Cumlaude">
+								<input type="text" class="form-control educ-value" data-column="honor_received"
+									name='honor_received' placeholder="Cumlaude">
 							</div>
 						</div>
 						<div class="col-xl-6 col-lg-6">
 							<div class="form-group">
 								<label>Year Start</label>
-								<input type="number" class="form-control" name='year_enrolled' required>
+								<input type="number" class="form-control educ-value" data-column="year_enrolled"
+									name='year_enrolled' required>
 							</div>
 						</div>
 						<div class="col-xl-6 col-lg-6">
 							<div class="form-group">
 								<label>Year End</label>
-								<input type="number" class="form-control" name='year_graduated' required>
+								<input type="number" class="form-control educ-value" data-column="year_graduated"
+									name='year_graduated' required>
 							</div>
 						</div>
 					</div>
@@ -190,36 +194,52 @@ AUTO_INCREMENT=3
 			success: function(data) {
 				$("#tbl_educ tbody").html("");
 				$.each(data.alumni, function(index, element) {
-					$("#tbl_educ tbody").append("<tr><td>" + element.educ_id + "</td><td>" + element.educ_degree + "</td><td>" + element.educ_school + "</td><td>" + element.honor_received + "</td><td>" + element.year_enrolled + " - " + element.year_graduated + "</td><td><button class='btn btn-xs btn-primary' onclick='editEduc()'><span class='fa fa-edit'></span></button> <button class='btn btn-xs btn-danger' onclick='deleteEduc("+element.educ_id+")'><span class='fa fa-trash'></span></button></td></tr>");
+					$("#tbl_educ tbody").append("<tr><td>" + element.educ_id + "</td><td>" + element.educ_degree + "</td><td>" + element.educ_school + "</td><td>" + element.honor_received + "</td><td>" + element.year_enrolled + " - " + element.year_graduated + "</td>" +
+						"<td><button class='btn btn-xs btn-primary' onclick='editEduc(" + JSON.stringify(element) + ")'  data-toggle='modal' data-target='#modalAddEduc'><span class='fa fa-edit'></span></button> <button class='btn btn-xs btn-danger' onclick='deleteEduc(" + element.educ_id + ")'><span class='fa fa-trash'></span></button></td></tr>");
 				});
 			}
 		});
 	}
 
-	function editEduc(){
-		modal_educ.classList.toggle("show-modal");
+	function editEduc(res) {
+		const profileValueElements = document.querySelectorAll('.educ-value');
+
+		// Loop through each element and retrieve the value of the "data-column" attribute
+		profileValueElements.forEach(element => {
+			const dataColumnValue = element.getAttribute('data-column');
+			element.value = res[dataColumnValue];
+		});
 	}
 
-	function deleteEduc(educ_id){
+	function deleteEduc(educ_id) {
 		Swal.fire({
-		  title: 'Are you sure?',
-		  text: "You won't be able to revert this!",
-		  icon: 'warning',
-		  showCancelButton: true,
-		  confirmButtonColor: '#3085d6',
-		  cancelButtonColor: '#d33',
-		  confirmButtonText: 'Yes, delete it!'
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!'
 		}).then((result) => {
-		  if (result.isConfirmed) {
-			$.post(base_controller + "delete_alumni_education", {educ_id:educ_id}, function(data, status) {
-				get_educations();
-			    Swal.fire(
-			      'Deleted!',
-			      'Your file has been deleted.',
-			      'success'
-			    )
-			});
-		  }
+			if (result.isConfirmed) {
+				$.post(base_controller + "delete_alumni_education", { educ_id: educ_id }, function(data, status) {
+					get_educations();
+					Swal.fire(
+						'Deleted!',
+						'Your file has been deleted.',
+						'success'
+					)
+				});
+			}
+		});
+	}
+
+	function resetFields() {
+		const profileValueElements = document.querySelectorAll('.educ-value');
+
+		// Loop through each element and retrieve the value of the "data-column" attribute
+		profileValueElements.forEach(element => {
+			element.value = "";
 		});
 	}
 </script>
