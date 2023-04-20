77e463e7e703534725ed8f5e33e2e7a13b286938
Eduard  Rino Carton <56856915+awtsmahalko@users.noreply.github.com>
2023-04-20 13:45:53 +0800

    Update routes.php

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -4,6 +4,6 @@ $requestPath = str_replace(HTACCESS_APP, "", $_SERVER['REQUEST_URI']);
 $request_path = $requestPath != '' ? $requestPath : 'dashboard';
 
 $route_param = explode("/", $request_path);
-
+echo $route_param[0];
 $router = new Router();
 $router->dispatch($route_param[0], $route_param);
